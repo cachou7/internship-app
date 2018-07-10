fb1167b6547aef96aa3f61385daa373d102878f6
Connor Eschrich <ceschrich42@yahoo.com>
2018-07-10 12:50:48 -0500

    initiative validation

--- PLC/InitiativeCreateViewController.swift
+++ PLC/InitiativeCreateViewController.swift
@@ -17,6 +17,11 @@ class InitiativeCreateViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var timeTextField: UITextField!
     @IBOutlet weak var bigIdeaButton: UIButton!
     @IBOutlet weak var communityButton: UIButton!
+    @IBOutlet weak var leadAmountTextField: UITextField!
+    @IBOutlet weak var participateAmountTextField: UITextField!
+    
+    @IBOutlet weak var validationButtonLabel: UILabel!
+    @IBOutlet weak var validationCheckBoxLabel: UILabel!
     
     let datePicker = UIDatePicker()
     var task: Task?
@@ -24,17 +29,26 @@ class InitiativeCreateViewController: UIViewController, UITextFieldDelegate {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        validationCheckBoxLabel.isEnabled = false
+        validationButtonLabel.isEnabled = false
+        bigIdeaButton.isSelected = false
+        communityButton.isSelected = false
+        leadAmountTextField.isEnabled = false
+        participateAmountTextField.isEnabled = false
+        
+        
         datePicker.datePickerMode = UIDatePickerMode.dateAndTime
         timeTextField.inputView = datePicker
         datePicker.addTarget(self, action: #selector(datePickerChanged), for:UIControlEvents.valueChanged)
-        bigIdeaButton.isSelected = false
-        communityButton.isSelected = false
+        
     }
     
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
     @IBAction func bigIdeaAction(_ sender: UIButton) {
         communityButton.isSelected = false
         bigIdeaButton.isSelected = true
@@ -51,10 +65,22 @@ class InitiativeCreateViewController: UIViewController, UITextFieldDelegate {
     
     @IBAction func leadCheckBox(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
+        if (sender.isSelected){
+            leadAmountTextField.isEnabled = true
+        }
+        else{
+            leadAmountTextField.isEnabled = false
+        }
     }
     
     @IBAction func participateCheckBox(_ sender: UIButton) {
         sender.isSelected = !sender.isSelected
+        if (sender.isSelected){
+            participateAmountTextField.isEnabled = true
+        }
+        else{
+            participateAmountTextField.isEnabled = false
+        }
     }
     
     
@@ -63,47 +89,47 @@ class InitiativeCreateViewController: UIViewController, UITextFieldDelegate {
     }
     
     @IBAction func createButton(_ sender: UIBarButtonItem) {
-        var tagResult: String = ""
-        var type: String = ""
-        if leadCheck.isSelected {
-            if tagResult == ""{
-                tagResult.append("#lead")
+        let valid = validate()
+        if (valid){
+            var tagResult: String = ""
+            var type: String = ""
+            if leadCheck.isSelected {
+                if tagResult == ""{
+                    tagResult.append("#lead")
+                }
+                else{
+                    tagResult.append(" #lead")
+                }
+            }
+            if participateCheck.isSelected {
+                if tagResult == ""{
+                    tagResult.append("#participate")
+                }
+                else{
+                    tagResult.append(" #participate")
+                }
+            }
+            if bigIdeaButton.isSelected {
+                type = "Big Idea"
             }
             else{
-                tagResult.append(" #lead")
+                type = "Community"
             }
-        }
-        if participateCheck.isSelected {
-            if tagResult == ""{
-                tagResult.append("#participate")
-            }
-            else{
-                tagResult.append(" #participate")
-            }
-        }
-        print(bigIdeaButton.isSelected)
-        print(communityButton.isSelected)
-        if bigIdeaButton.isSelected {
-            type = "Big Idea"
-        }
-        else if communityButton.isSelected {
-            type = "Community"
+            let interval = NSDate().timeIntervalSince1970
+            let key = Constants.refs.databaseTasks.childByAutoId().key
+            
+            task = Task(title: titleTextField.text!, description: descriptionTextField.text!, tag: tagResult, time: timeTextField.text!, location: locationTextField.text!, timestamp: String(interval), id: key, createdBy: currentUser.uid, ranking: "0", timeMilliseconds: eventTime!, type: type)
+            
+            let taskDB = ["taskId": key, "taskTitle": task?.title, "taskDescription": task?.description, "taskTag": task?.tag, "taskTime": task?.time, "taskLocation": task?.location, "timestamp": task?.timestamp, "createdBy" : task?.createdBy, "ranking": task?.ranking, "taskTimeMilliseconds": task?.timeMilliseconds, "taskType": task?.type]
+            Constants.refs.databaseTasks.child(key).setValue(taskDB)
+            let tasksCreated = Constants.refs.databaseUsers.child(currentUser.uid + "/tasks_created")
+            tasksCreated.child("taskId").setValue(key)
+            print("Task added")
+            dismiss()
         }
         else{
-            let alert = UIAlertController(title: "No Task Type", message: "Please select Big Idea or Community", preferredStyle: .alert)
-            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            
         }
-        let interval = NSDate().timeIntervalSince1970
-        let key = Constants.refs.databaseTasks.childByAutoId().key
-        
-        task = Task(title: titleTextField.text!, description: descriptionTextField.text!, tag: tagResult, time: timeTextField.text!, location: locationTextField.text!, timestamp: String(interval), id: key, createdBy: currentUser.uid, ranking: "0", timeMilliseconds: eventTime!, type: type)
-        
-        let taskDB = ["taskId": key, "taskTitle": task?.title, "taskDescription": task?.description, "taskTag": task?.tag, "taskTime": task?.time, "taskLocation": task?.location, "timestamp": task?.timestamp, "createdBy" : task?.createdBy, "ranking": task?.ranking, "taskTimeMilliseconds": task?.timeMilliseconds, "taskType": task?.type]
-        Constants.refs.databaseTasks.child(key).setValue(taskDB)
-        let tasksCreated = Constants.refs.databaseUsers.child(currentUser.uid + "/tasks_created")
-        tasksCreated.child("taskId").setValue(key)
-        print("Task added")
-        dismiss()
     }
     
     @objc func datePickerChanged(datePicker:UIDatePicker) {
@@ -125,6 +151,52 @@ class InitiativeCreateViewController: UIViewController, UITextFieldDelegate {
     
     private func dismiss(){
         self.dismiss(animated: true, completion: nil)
+    }
+    
+    private func validate() -> Bool{
+        var valid:Bool = true
+        if (titleTextField.text?.isEmpty)! {
+            //Change the placeholder color to red for textfield email if
+            titleTextField.attributedPlaceholder = NSAttributedString(string: "Please enter Task Title", attributes: [NSAttributedStringKey.foregroundColor: UIColor.red])
+            valid = false
+        }
+        if (descriptionTextField.text?.isEmpty)!{
+            // Change the placeholder color to red for textfield userName
+            descriptionTextField.attributedPlaceholder = NSAttributedString(string: "Please enter Task Description", attributes: [NSAttributedStringKey.foregroundColor: UIColor.red])
+            valid = false
+        }
+        if (timeTextField.text?.isEmpty)!{
+            // Change the placeholder color to red for textfield passWord
+            timeTextField.attributedPlaceholder = NSAttributedString(string: "Please enter a Time", attributes: [NSAttributedStringKey.foregroundColor: UIColor.red])
+            valid = false
+        }
+        if (locationTextField.text?.isEmpty)!{
+            // Change the placeholder color to red for textfield passWord
+            locationTextField.attributedPlaceholder = NSAttributedString(string: "Please enter a Location", attributes: [NSAttributedStringKey.foregroundColor: UIColor.red])
+            valid = false
+        }
+        if !(leadCheck.isSelected) && !(participateCheck.isSelected){
+            validationCheckBoxLabel.isEnabled = true
+            validationCheckBoxLabel.attributedText = NSAttributedString(string: "Please select '#lead' and/or '#participate'", attributes: [NSAttributedStringKey.foregroundColor: UIColor.red])
+            valid = false
+        }
+        if !(bigIdeaButton.isSelected) && !(communityButton.isSelected){
+            validationButtonLabel.isEnabled = true
+            validationButtonLabel.attributedText = NSAttributedString(string: "Please select 'Big Idea' or 'Community'", attributes: [NSAttributedStringKey.foregroundColor: UIColor.red])
+            valid = false
+        }
+        else{
+            validationButtonLabel.text?.removeAll()
+            validationButtonLabel.isEnabled = false
+        }
+        if ((leadAmountTextField.isEnabled) && (leadAmountTextField.text?.isEmpty)!) || ((participateAmountTextField.isEnabled) && (participateAmountTextField.text?.isEmpty)!){
+            validationCheckBoxLabel.attributedText = NSAttributedString(string: "Please enter amount of task leaders/participants", attributes: [NSAttributedStringKey.foregroundColor: UIColor.red])
+        }
+        else{
+            validationCheckBoxLabel.text?.removeAll()
+            validationCheckBoxLabel.isEnabled = false
+        }
+        return valid
     }
     
     
