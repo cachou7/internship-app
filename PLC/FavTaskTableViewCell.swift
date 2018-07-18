5ab289ac4a2b2797d022db3f78b3c3461a5645ed
Chris <cachou@umich.edu>
2018-07-18 16:06:09 -0500

    updated .gitignore

---
+++ PLC/FavTaskTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FavTaskTableViewCell.swift
+//  PLC
+//
+//  Created by Chris on 7/18/18.
+//  Copyright © 2018 Chris Chou. All rights reserved.
+//
+
+import UIKit
+
+class FavTaskTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
